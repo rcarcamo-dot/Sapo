3b4934a5743f0a27266eb69f85f3a5c32f9af4a5
rcarcamo-dot <74024537+rcarcamo-dot@users.noreply.github.com>
2021-09-27 10:41:16 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,104 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(Biostrings)
+library(crisscrosslinker)
+library(ape)
+library(phangorn)
+library(seqinr)
+library(msa)
+library(tidyverse)
+library(ggplot2)
+library(ggtree)
+library(ggrepel)
+library(treeio)
+
+shinyServer(function(input,output,session){
+
+    query <- reactive(input$txt)
+    myquerypath <- file.path("../fastas/query.fa")
+    if (startsWith(query, ">")){
+        writeLines(query, myquerypath)
+    } else {
+        writeLines(paste0(">Query\n",query), myquerypath)
+    }
+    
+    
+  
+    #this makes sure the fasta is formatted properly
+   
+    results_folder <- file.path("../results")
+    myquerypath <- file.path("../fastas/query.fa")
+    query <-read.fasta(file = myquerypath, seqtype = "DNA",as.string = TRUE)
+    myquery <- readLines(myquerypath)
+    header <- gsub(">", "", myquery[grep("^>", myquery)])
+    query_length <- getLength(query)
+    querystring <- toupper(c2s(query[[1]]))  #extract query sequence and capitalize it
+    #Load templates fasta files
+    gi_1_templates <- read.fasta (file = '../fastas/GI_1.fa', seqtype = "DNA", as.string = TRUE)
+    gi_2_templates <- read.fasta (file = '../fastas/GI_2.fa', seqtype = "DNA", as.string = TRUE)
+    gii_1_templates <- read.fasta (file = '../fastas/GII_1.fa', seqtype = "DNA", as.string = TRUE)
+    gii_3_templates <- read.fasta (file = '../fastas/GII_3.fa', seqtype = "DNA", as.string = TRUE)
+    gii_4_templates <- read.fasta (file = '../fastas/GII_4.fa', seqtype = "DNA", as.string = TRUE)
+    gii_5_templates <- read.fasta (file = '../fastas/GII_5.fa', seqtype = "DNA", as.string = TRUE)
+    gv_i_templates <- read.fasta (file = '../fastas/GV_1.fa', seqtype = "DNA", as.string = TRUE)
+    
+    #List of the files that contain templates for each genotype
+    list_files <- c(gi_1_templates, gi_2_templates, gii_1_templates, gii_3_templates, gii_4_templates, gii_5_templates, gv_i_templates)
+    
+    
+    list_genotypes <- c('../fastas/GI_1.fa', '../fastas/GI_2.fa', '../fastas/GII_1.fa', '../fastas/GII_3.fa', '../fastas/GII_4.fa', '../fastas/GII_5.fa', '../fastas/GV_1.fa')
+    
+    
+    #This iterates 7 times over each file, but we have to manage how to print the messages without this loop
+    for (r in 1:length(list_genotypes)){ #Iterate through all the template files
+      list_templates <- list()
+      my_positive_string <- paste("This sequence belongs to", substr(list_genotypes[[r]], start = 11, stop = 15), "genotype.", sep = " ")
+      my_negative_string <- paste("This sequence does not belong to", substr(list_genotypes[[r]], start = 11, stop = 15), "genotype.", sep = " ")
+      for(i in 1:length(list_files)) { 
+        nam <- paste("seq", i, sep = "")
+        assign(nam, toupper(c2s(list_files[[i]])))
+        list_templates[[nam]] <- (c2s(list_files[[i]]))
+        write.fasta(sequences= toupper(c2s(list_files[[i]])), names=nam, file.out="../fastas/template.fa")
+        fasta_file_names <- c(myquerypath, '../fastas//template.fa')
+        combined_fasta <- fasta.combine(fasta_file_names = fasta_file_names)
+        write.fasta(sequences=combined_fasta,names=names(combined_fasta),file.out="../fastas/query_template.fa")
+        query_template <- file.path("../fastas/query_template.fa")
+        query_template = readLines("../fastas/query_template.fa")
+        query_template = toupper(query_template)
+        writeLines(query_template, "../fastas/query_template.fa")
+        mysequences = readDNAStringSet("../fastas/query_template.fa")
+        #print(toupper(mysequences))
+        myClustalWAlignment <- msa(mysequences, "ClustalW", type = "dna")
+        myClustalWAlignment <- msaConvert(myClustalWAlignment, "seqinr::alignment")
+        d <- dist.alignment(myClustalWAlignment, matrix = "identity")
+        print(d)
+        if (d <= 0.169){
+          genotype <- my_positive_string
+          break
+        } else {
+          print (my_negative_string, quote = FALSE)
+          next
+        }
+      }
+    }
+    
+    summary_table <- data.frame(
+      Name    = header,
+      Length  = query_length,
+      Genotype = genotype
+    )
+    summary_table
+    
+  })# sanitize.text.function = function(x) x)
+  
+  #output$phylo <- renderplot({ })
+#})
+
---
+++ ui.R
@@ -0,0 +1,55 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    # App title ----
+    titlePanel("Sapoapp"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            radioButtons("radioButtons", 
+                               h3("Select input format"), 
+                               choices = list("Upload FASTA file" = 1, 
+                                              "Paste sequence" = 2),
+                               selected = 0),
+            
+            conditionalPanel(
+                condition ="input.radioButtons=='1'",
+                fileInput("file1", h3("Choose Fasta File"),
+                          multiple = TRUE,
+                          accept = c("text/fasta",
+                                     ".fa",
+                                     ".fasta"))
+                ),
+            
+            conditionalPanel(
+                condition ="input.radioButtons=='2'",
+                #selectizeInput("text", h3("Input words here"), 
+                               #choices = NULL, 
+                               #multiple = FALSE, 
+                               #options = list(create = TRUE)),
+                textAreaInput('txt', h3('Input sequence:'), value = "", 
+                              placeholder = "", width = "600px", height="200px"),
+                tags$hr(),
+                
+                
+                
+                actionButton(inputId = "searchButton", label = "Run")
+            )
+            
+            
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Genotype", tableOutput("summary_table") ), 
+                tabPanel("Phylogenetic analysis", verbatimTextOutput("summary"))
+            )
+        
+    )
+)))
+
